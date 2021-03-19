f3dff577638a99e42fb405b2f11989b87192bdfa
ericminhas <sericminhas@gmail.com>
2021-03-18 18:15:25 -0600

    thursday

---
+++ javascript/1.0 Java Syntax and TDD/syntax.js
@@ -0,0 +1,41 @@
+//
+const functions = {
+
+    add: (num1,num2) => {
+        return (num1 + num2) 
+    }
+
+    // Define some attributes 
+        //Number - a number stored inside the file and doesn't define the different types of numbers (integers, short, long etc)
+        //String - pretty much just text (including spaces)
+        //Boolean - wants to know true or false (definites)
+        //Array - a special *variable* that holds data in a specific structure 0,1,2,3 list 
+        //Dictionary/Object - a special *variable* which also holds data in a specfic structure, we define the first part of the list 
+        //undefined - a variable that has not been assigned a value or not declared 
+
+    // If/Else loops - a conditional statement for arriving to a desired outcome 
+    
+    //functions - parameters = simply the names listed in the function definition in the brackets -- 
+    //argument - the specific value we are passing as the parameter 
+
+    //arrays 
+        //add to the front -- use unshift method 
+        //add to the back -- use push method 
+        //
+
+    //loops
+        // for loop - loops through a block of code 
+        //for in - loops through properties of ab object 
+        // while -- loops through a block of code while a specified condition is true 
+        //do while - loops through a block of code once - then repeats when a specified condition is true 
+        //forEach -- uses a anonymous (automatic) function for every single element for an array 
+    //object - dictionary 
+        //delcare object - use notation {}
+        //lookup key to retive value -- example 
+            // var person = {name:john}
+            // person.name  will then return john 
+
+
+}
+
+export default functions;
